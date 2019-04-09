fbffe25b26493d61216a549e5c934d88c52daece
Allan Ritchie <allan.ritchie@gmail.com>
2019-04-09 08:23:56 -0400

    Add project files.

---
+++ Samples.Android/MainApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Shiny;
+using Android.App;
+using Android.Runtime;
+using Samples.ShinySetup;
+
+
+namespace Samples.Droid
+{
+    [Application]
+    public class MainApplication : Application
+    {
+        public MainApplication() : base() { }
+        public MainApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
+        {
+        }
+
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            AndroidShinyHost.Init(this, new SampleStartup());
+        }
+    }
+}
---
+++ Samples/Notifications/SettingsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+
+namespace Samples.Notifications
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
