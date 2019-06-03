734caa55af5a9875cccbdd6f45d9e87af1aa64d6
Allan Ritchie <allan.ritchie@gmail.com>
2019-06-03 09:55:45 -0400

    minor

--- Samples/Beacons/MonitoringViewModel.cs
+++ Samples/Beacons/MonitoringViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Threading.Tasks;
 using System.Windows.Input;
 using Acr.UserDialogs.Forms;
 using Prism.Navigation;
