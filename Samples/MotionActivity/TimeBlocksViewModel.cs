78724e3cb8959fb335d46e855cb28c899c8d0c14
Allan Ritchie <allan.ritchie@gmail.com>
2019-09-13 14:25:36 -0400

    additional updates to motion activity screens

--- Samples/MotionActivity/TimeBlocksViewModel.cs
+++ Samples/MotionActivity/TimeBlocksViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
@@ -20,16 +23,26 @@ namespace Samples.MotionActivity
                     .OrderBy(x => x.Start)
                     .Select(x => new CommandItem
                     {
-                        Text = $"{x.Start} - {x.End}",
+                        Text = $"{x.Start.LocalDateTime.TimeOfDay} - {x.End.LocalDateTime.TimeOfDay}",
                         Detail = x.Type.ToString()
                     })
                     .ToList();
             });
             this.BindBusyCommand(this.Load);
             this.WhenAnyValue(x => x.Date)
-                .InvokeCommand((ICommand)this.Load);
+                .Skip(1)
+                .DistinctUntilChanged()
+                .Select(_ => Unit.Default)
+                .InvokeCommand((ICommand)this.Load)
+                .DisposeWith(this.DestroyWith);
         }
 
+
+        public override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((ICommand)this.Load).Execute(null);
+        }
 
         public IReactiveCommand Load { get; }
         [Reactive] public DateTime Date { get; set; } = DateTime.Now;
--- Samples/MotionActivity/TotalTimesViewModel.cs
+++ Samples/MotionActivity/TotalTimesViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
@@ -27,9 +30,19 @@ namespace Samples.MotionActivity
             });
             this.BindBusyCommand(this.Load);
             this.WhenAnyValue(x => x.Date)
-                .InvokeCommand((ICommand)this.Load);
+                .Skip(1)
+                .DistinctUntilChanged()
+                .Select(_ => Unit.Default)
+                .InvokeCommand((ICommand)this.Load)
+                .DisposeWith(this.DestroyWith);
         }
 
+
+        public override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((ICommand)this.Load).Execute(null);
+        }
 
         public IReactiveCommand Load { get; }
         [Reactive] public DateTime Date { get; set; } = DateTime.Now;
