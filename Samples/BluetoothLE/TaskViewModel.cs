c9115df002e914680bcb75991ed56f5ef7cf0f8b
Allan Ritchie <allan.ritchie@gmail.com>
2019-10-09 21:43:19 -0400

    minor

--- Samples/BluetoothLE/TaskViewModel.cs
+++ Samples/BluetoothLE/TaskViewModel.cs
@@ -17,45 +17,29 @@ namespace Samples.BluetoothLE
         {
             this.Text = text;
 
-            this.WhenAnyValue(x => x.IsBusy)
-                .Select(x => x ? "Stop" : "Start")
-                .ToPropertyEx(this, x => x.ButtonText);
-
-            this.Run = ReactiveCommand.CreateFromTask(async () =>
-            {
-                if (this.cancelSrc == null)
+            this.Start = ReactiveCommand.CreateFromTask(
+                async () =>
                 {
                     this.cancelSrc = new CancellationTokenSource();
                     await run(this.cancelSrc.Token);
-                }
-                else
-                {
-                    this.Cancel();
-                }
-            }, whenAny);
+                },
+                whenAny
+            );
+            this.BindBusyCommand(this.Start);
 
-            this.BindBusyCommand(this.Run);
-        }
-
-
-        public IReactiveCommand Run { get; }
-        public string Text { get; }
-        public string ButtonText { [ObservableAsProperty] get; }
-
-
-        public void Cancel()
-        {
-            try
+            this.Stop = ReactiveCommand.Create(() =>
             {
                 this.IsBusy = false;
                 this.cancelSrc?.Cancel();
                 this.cancelSrc = null;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
+            });
         }
+
+
+        public IReactiveCommand Start { get; }
+        public IReactiveCommand Stop { get; }
+        public string Text { get; }
+        public string ButtonText { [ObservableAsProperty] get; }
     }
 }
 
