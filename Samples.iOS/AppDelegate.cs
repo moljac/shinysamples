8fafe59971097919dde18adefffb265c50145a3d
Allan Ritchie <allan.ritchie@gmail.com>
2019-11-24 09:07:24 -0500

    start push wiring

--- Samples.iOS/AppDelegate.cs
+++ Samples.iOS/AppDelegate.cs
@@ -4,11 +4,12 @@ using UIKit;
 using Xamarin;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
+using Acr.UserDialogs.Forms;
 using Samples.ShinySetup;
 using Shiny;
 using Shiny.Net.Http;
 using Shiny.Jobs;
-using Acr.UserDialogs.Forms;
+using Shiny.Push;
 
 
 namespace Samples.iOS
@@ -29,6 +30,14 @@ namespace Samples.iOS
             this.LoadApplication(new App());
             return base.FinishedLaunching(app, options);
         }
+
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+            => PushNotificationManager.RegisteredForRemoteNotifications(deviceToken);
+
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+            => PushNotificationManager.FailedToRegisterForRemoteNotifications(error);
 
 
         // if you are using jobs, you need this
