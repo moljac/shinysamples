31c2b2712dcd50f925e9f9bd4dc8f4e3d68c1f2f
Allan Ritchie <allan.ritchie@gmail.com>
2019-12-09 21:19:20 -0500

    Update JobLoggerTask.cs

--- Samples/ShinySetup/JobLoggerTask.cs
+++ Samples/ShinySetup/JobLoggerTask.cs
@@ -26,28 +26,22 @@ namespace Samples.ShinySetup
 
         public void Start()
         {
-            this.jobManager.JobStarted += async (sender, args) =>
+            this.jobManager.JobStarted.SubscribeAsync(args => this.conn.InsertAsync(new JobLog
             {
-                await this.conn.InsertAsync(new JobLog
-                {
-                    JobIdentifier = args.Identifier,
-                    JobType = args.Type.FullName,
-                    Started = true,
-                    Timestamp = DateTime.Now,
-                    Parameters = this.serializer.Serialize(args.Parameters)
-                });
-            };
-            this.jobManager.JobFinished += async (sender, args) =>
+                JobIdentifier = args.Identifier,
+                JobType = args.Type.FullName,
+                Started = true,
+                Timestamp = DateTime.Now,
+                Parameters = this.serializer.Serialize(args.Parameters)
+            }));
+            this.jobManager.JobFinished.SubscribeAsync(args => this.conn.InsertAsync(new JobLog
             {
-                await this.conn.InsertAsync(new JobLog
-                {
-                    JobIdentifier = args.Job.Identifier,
-                    JobType = args.Job.Type.FullName,
-                    Error = args.Exception?.ToString(),
-                    Parameters = this.serializer.Serialize(args.Job.Parameters),
-                    Timestamp = DateTime.Now
-                });
-            };
+                JobIdentifier = args.Job.Identifier,
+                JobType = args.Job.Type.FullName,
+                Error = args.Exception?.ToString(),
+                Parameters = this.serializer.Serialize(args.Job.Parameters),
+                Timestamp = DateTime.Now
+            }));
         }
     }
 }
