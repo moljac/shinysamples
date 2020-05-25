fc6adfb1dd41480f405c897cea5b985a14a81c2d
Allan Ritchie <allan.ritchie@gmail.com>
2020-05-25 08:52:44 -0400

    Fix compile

--- Samples/LocationSync/MainPage.xaml.cs
+++ Samples/LocationSync/MainPage.xaml.cs
@@ -1,20 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+
 
 namespace Samples.LocationSync
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
