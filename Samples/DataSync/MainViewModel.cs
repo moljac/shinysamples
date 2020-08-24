00aec9fee286c6a5846014669972e8e6689b0d1b
Allan Ritchie <allan.ritchie@gmail.com>
2020-08-24 10:45:31 -0400

    Fix samples

--- Samples/DataSync/MainViewModel.cs
+++ Samples/DataSync/MainViewModel.cs
@@ -1,14 +1,117 @@
-﻿using Shiny.DataSync;
-using System;
+﻿using System;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Shiny;
+using Shiny.DataSync;
+using Samples.Infrastructure;
+using Prism.Navigation;
 
 
 namespace Samples.DataSync
 {
     public class MainViewModel : ViewModel
     {
-        public MainViewModel(IDataSyncManager manager)
-        {
+        readonly IDataSyncManager manager;
+        readonly INavigationService navigator;
 
+
+        public MainViewModel(IDataSyncManager manager,
+                             SampleDataSyncDelegate sdelegate,
+                             INavigationService navigator,
+                             IDialogs dialogs,
+                             Shiny.IMessageBus messageBus)
+        {
+            this.manager = manager;
+            this.navigator = navigator;
+
+            this.IsSyncEnabled = manager.Enabled;
+            this.AllowOutgoing = sdelegate.AllowOutgoing;
+
+            this.GenerateTestItem = ReactiveCommand.CreateFromTask(() =>
+                dialogs.LoadingTask(() => manager.ForceRun())
+            );
+
+            this.ForceRun = ReactiveCommand.CreateFromTask(() =>
+                dialogs.LoadingTask(() => manager.ForceRun())
+            );
+
+            this.Clear = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var result = await dialogs.Confirm("Are you sure you wish to clear the data sync pending queue?");
+                if (result)
+                {
+                    await this.manager.ClearPending();
+                    await this.BindList();
+                }
+            });
+
+            this.WhenAnyValue(x => x.AllowOutgoing)
+                .Skip(1)
+                .Subscribe(x => sdelegate.AllowOutgoing = x)
+                .DisposeWith(this.DestroyWith);
+
+            this.WhenAnyValue(x => x.IsSyncEnabled)
+                .Skip(1)
+                .Subscribe(x => manager.Enabled = x)
+                .DisposeWith(this.DestroyWith);
+
+            messageBus
+                .Listener<SyncItem>()
+                .SubOnMainThread(x => this.Remove(x.Id))
+                .DisposeWith(this.DestroyWith);
+        }
+
+
+        public ICommand GenerateTestItem { get; }
+        public ICommand ForceRun { get; }
+        public ICommand Clear { get; }
+        public ObservableList<CommandItem> SyncEvents { get; } = new ObservableList<CommandItem>();
+        [Reactive] public bool IsSyncEnabled { get; set; }
+        [Reactive] public bool AllowOutgoing { get; set; }
+
+
+        public override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await this.BindList();
+        }
+
+
+        async Task Remove(Guid syncId)
+        {
+            var e = this.SyncEvents.FirstOrDefault(y => ((SyncItem)y.Data).Id == syncId);
+            if (e != null)
+                this.SyncEvents.Remove(e);
+        }
+
+
+        async Task BindList()
+        {
+            var items = await this.manager.GetPendingItems();
+            var commands = items
+                .Select(x => new CommandItem
+                {
+                    Text = $"[{x.Operation}] {x.TypeName}:{x.EntityId}",
+                    Detail = $"[{x.Operation}]",
+                    Data = x,
+                    PrimaryCommand = ReactiveCommand.CreateFromTask(() =>
+                        this.navigator.ShowBigText(
+                            x.SerializedEntity,
+                            "Data Sync"
+                        )
+                    ),
+                    SecondaryCommand = ReactiveCommand.CreateFromTask(() =>
+                        this.manager.Remove(x.Id)
+                    )
+                })
+                .ToList();
+
+            this.SyncEvents.ReplaceAll(commands);
         }
     }
 }
--- Samples/DataSync/SampleDataSyncDelegate.cs
+++ Samples/DataSync/SampleDataSyncDelegate.cs
@@ -1,15 +1,36 @@
-﻿using Shiny.DataSync;
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Shiny.DataSync;
 using System;
 using System.Threading.Tasks;
 
 
 namespace Samples.DataSync
 {
-    public class SampleDataSyncDelegate : IDataSyncDelegate
+    public class SampleDataSyncDelegate : ReactiveObject, IDataSyncDelegate
     {
-        public Task Push(SyncItem item)
+        readonly Shiny.IMessageBus messageBus;
+
+
+        public SampleDataSyncDelegate(Shiny.IMessageBus messageBus)
         {
-            return Task.CompletedTask;
+            this.messageBus = messageBus;
+        }
+
+
+        /// <summary>
+        /// This is used to show failures in the your sync process
+        /// </summary>
+        [Reactive] public bool AllowOutgoing { get; set; } = true;
+
+
+        public async Task Push(SyncItem item)
+        {
+            await Task.Delay(1000); // simulate a delay to an http call
+            if (!this.AllowOutgoing)
+                throw new ArgumentException("Testing sync failure");
+
+            this.messageBus.Publish(item);
         }
     }
 }
