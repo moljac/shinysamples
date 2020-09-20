a508165b1703636bab1af8f09704d6aebd851e3a
Allan Ritchie <allan.ritchie@gmail.com>
2020-09-20 13:42:29 -0400

    Improved notification setup

--- Samples/AppNotifications.cs
+++ Samples/AppNotifications.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Shiny.Notifications;
 using Shiny.Settings;
@@ -6,33 +8,59 @@ using Shiny.Settings;
 
 namespace Samples
 {
+    public class NotificationRegistration
+    {
+        public NotificationRegistration(string description, Type type, bool hasEntryExit)
+        {
+            this.Description = description;
+            this.Type = type;
+            this.HasEntryExit = hasEntryExit;
+        }
+
+
+        public string Description { get; }
+        public Type Type { get; }
+        public bool HasEntryExit { get; }
+    }
+
+
     public class AppNotifications
     {
         readonly ISettings settings;
         readonly INotificationManager notifications;
+        readonly Dictionary<Type, NotificationRegistration> registrations;
 
 
         public AppNotifications(ISettings settings, INotificationManager notifications)
         {
             this.settings = settings;
             this.notifications = notifications;
+            this.registrations = new Dictionary<Type, NotificationRegistration>();
         }
+
+
+        public NotificationRegistration[] GetRegistrations() => this.registrations.Values.ToArray();
 
 
         public void Set(Type type, bool entry, bool enabled)
             => this.settings.Set(ToKey(type, entry), enabled);
 
 
-        //public void Register(Type type, bool hasEntryExit, string description)
-        //{
+        public void Register(Type type, bool hasEntryExit, string description)
+        {
+            if (this.registrations.ContainsKey(type))
+                throw new ArgumentException("Already registrated");
 
-        //}
+            this.registrations.Add(type, new NotificationRegistration(description, type, hasEntryExit));
+        }
+
+
+        public bool IsEnabled(Type type, bool entry) => this.settings.Get(ToKey(type, entry), true);
 
 
         public async Task Send(Type type, bool entry, string title, string message)
         {
-            var enabled = this.settings.Get(ToKey(type, entry), true);
-            if (enabled)
+            if (IsEnabled(type, entry))
                 await this.notifications.Send(title, message);
         }
 
--- Samples/DelegateNotificationsViewModel.cs
+++ Samples/DelegateNotificationsViewModel.cs
@@ -5,32 +5,38 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
-using Samples.Settings;
-using Shiny;
-using Shiny.Settings;
 
 
 namespace Samples
 {
     public class DelegateNotificationsViewModel : ViewModel
     {
-        public DelegateNotificationsViewModel(ISettings settings, IAppSettings appSettings)
+        public DelegateNotificationsViewModel(AppNotifications notifications)
         {
-            this.Notifications = new List<DelegateNotificationItemViewModel>
-            {
-                new DelegateNotificationItemViewModel(settings, nameof(BluetoothLE.BleClientDelegate), "BluetoothLE"),
-                new DelegateNotificationItemViewModel(settings, nameof(HttpTransfers.HttpTransferDelegate), "HTTP Transfers"),
-                new DelegateNotificationItemViewModel(settings, nameof(Beacons.BeaconDelegate) + "Entry", "Beacon Region Entry"),
-                new DelegateNotificationItemViewModel(settings, nameof(Beacons.BeaconDelegate) + "Exit", "Beacon Region Exit"),
-                new DelegateNotificationItemViewModel(settings, nameof(Geofences.GeofenceDelegate) + "Entry", "Geofence Entry"),
-                new DelegateNotificationItemViewModel(settings, nameof(Geofences.GeofenceDelegate) + "Exit", "Geofence Exit"),
-                new DelegateNotificationItemViewModel(settings, nameof(Jobs.JobLoggerTask) + "Start", "Job Started"),
-                new DelegateNotificationItemViewModel(settings, nameof(Jobs.JobLoggerTask) + "Finished", "Job Finished")
-            };
+
+            //this.Notifications = new List<DelegateNotificationItemViewModel>
+            //{
+            //    new DelegateNotificationItemViewModel(settings, nameof(BluetoothLE.BleClientDelegate), "BluetoothLE"),
+            //    new DelegateNotificationItemViewModel(settings, nameof(HttpTransfers.HttpTransferDelegate), "HTTP Transfers"),
+            //    new DelegateNotificationItemViewModel(settings, nameof(Beacons.BeaconDelegate) + "Entry", "Beacon Region Entry"),
+            //    new DelegateNotificationItemViewModel(settings, nameof(Beacons.BeaconDelegate) + "Exit", "Beacon Region Exit"),
+            //    new DelegateNotificationItemViewModel(settings, nameof(Geofences.GeofenceDelegate) + "Entry", "Geofence Entry"),
+            //    new DelegateNotificationItemViewModel(settings, nameof(Geofences.GeofenceDelegate) + "Exit", "Geofence Exit"),
+            //    new DelegateNotificationItemViewModel(settings, nameof(Jobs.JobLoggerTask) + "Start", "Job Started"),
+            //    new DelegateNotificationItemViewModel(settings, nameof(Jobs.JobLoggerTask) + "Finished", "Job Finished")
+            //};
+            this.Notifications = notifications
+                .GetRegistrations()
+                .Select(x => new DelegateNotificationItemViewModel(notifications, x))
+                .ToList();
 
             this.WhenAnyValue(x => x.ToggleAll)
                 .Skip(1)
-                .Subscribe(x => this.Notifications.ForEach(y => y.IsEnabled = x))
+                .Subscribe(x => this.Notifications.ForEach(y =>
+                {
+                    y.IsEntryEnabled = x;
+                    y.IsExitEnabled = x;
+                }))
                 .DisposeWith(this.DeactivateWith);
         }
 
@@ -42,20 +48,26 @@ namespace Samples
 
     public class DelegateNotificationItemViewModel : ReactiveObject
     {
-        public DelegateNotificationItemViewModel(ISettings settings, string key, string description)
+        public DelegateNotificationItemViewModel(AppNotifications notifications, NotificationRegistration reg)
         {
-            this.Key = key;
-            this.Description = description;
-            this.IsEnabled = settings.Get<bool>(key);
+            this.Description = reg.Description;
+            this.Text = reg.HasEntryExit ? "Entry" : "Enabled";
+            this.HasEntryExit = reg.HasEntryExit;
 
-            this.WhenAnyValue(x => x.IsEnabled)
+            this.WhenAnyValue(x => x.IsEntryEnabled)
                 .Skip(1)
-                .Subscribe(x => settings.Set(Key, x));
+                .Subscribe(x => notifications.Set(reg.Type, true, x));
+
+            this.WhenAnyValue(x => x.IsExitEnabled)
+                .Skip(1)
+                .Subscribe(x => notifications.Set(reg.Type, false, x));
         }
 
 
-        public string Key { get; }
         public string Description { get; }
-        public bool IsEnabled { get; set; }
+        public string Text { get; }
+        public bool HasEntryExit { get; }
+        [Reactive] public bool IsEntryEnabled { get; set; }
+        [Reactive] public bool IsExitEnabled { get; set; }
     }
 }
