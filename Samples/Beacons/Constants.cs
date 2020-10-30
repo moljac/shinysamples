32530f1416aa6f3e45816be92cda47763317e0d4
Allan Ritchie <allan.ritchie@gmail.com>
2020-10-30 11:08:09 -0400

    Minor

--- Samples/Beacons/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Samples.Beacons
-{
-    public static class Constants
-    {
-        public const string EstimoteUuid = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
-    }
-}
--- Samples/Constants.cs
+++ Samples/Constants.cs
@@ -5,6 +5,7 @@ namespace Samples
 {
     static class Constants
     {
+        public const string EstimoteUuid = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
 #if DEBUG
         public const string AppCenterTokens = "";
         public const string PhotoSyncUploadUri = "https://blah.com";
