4c0609641d2a1b76faf3251a7c2d381729e0e6a5
Allan Ritchie <allan.ritchie@gmail.com>
2020-12-17 20:29:14 -0500

    updates

--- Samples/Notifications/ChannelListViewModel.cs
+++ Samples/Notifications/ChannelListViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 using Prism.Navigation;
+using ReactiveUI;
 using Samples.Infrastructure;
 using Shiny.Notifications;
 
@@ -10,24 +12,45 @@ namespace Samples.Notifications
 {
     public class ChannelListViewModel : ViewModel
     {
-        readonly INotificationManager notifications;
-        readonly IDialogs dialogs;
-
-
-        public ChannelListViewModel(INavigationService navigator, INotificationManager notifications, IDialogs dialogs)
+        public ChannelListViewModel(INavigationService navigator,
+                                    INotificationManager notifications,
+                                    IDialogs dialogs)
         {
-            this.notifications = notifications;
-            this.dialogs = dialogs;
             this.Create = navigator.NavigateCommand("ChannelCreate");
+
+            this.LoadChannels = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var channels = await notifications.GetChannels();
+                this.Channels = channels
+                    .Select(x => new CommandItem
+                    {
+                        Text = x.Identifier,
+                        SecondaryCommand = ReactiveCommand.CreateFromTask(async () =>
+                        {
+                            var confirm = await dialogs.Confirm("Are you sure you wish to delete this channel?");
+                            if (confirm)
+                            {
+                                await notifications.DeleteChannel(x.Identifier);
+                                this.LoadChannels.Execute(null);
+                            }
+                        })
+                    })
+                    .ToList();
+
+                this.RaisePropertyChanged(nameof(this.Channels));
+            });
         }
 
 
         public ICommand Create { get; }
-        public IList<CommandItem> Channels { get; }
+        public ICommand LoadChannels { get; }
+        public IList<CommandItem> Channels { get; private set; }
+
 
         public override void OnAppearing()
         {
             base.OnAppearing();
+            this.LoadChannels.Execute(null);
         }
     }
 }
